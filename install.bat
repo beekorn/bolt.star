353e9e7a2ee153971c4a0188e21469a79c5a5e65
Beekorn <162875479+beekorn@users.noreply.github.com>
2025-02-22 11:44:01 +0800

    Add files via upload

--- install.bat
+++ install.bat
@@ -25,25 +25,5 @@ REM Install Remix dependencies explicitly
 echo Installing Remix dependencies...
 call npm install --save-dev @remix-run/dev@latest @remix-run/node@latest @remix-run/react@latest @remix-run/serve@latest
 
-REM Create the shortcut with full path displayed
-echo Creating Bolt shortcut...
-set "CURRENT_DIR=%CD%"
-echo Creating shortcut at: %CURRENT_DIR%\bolt.lnk
-echo Set oWS = WScript.CreateObject("WScript.Shell") > create_shortcut.vbs
-echo sLinkFile = "%CURRENT_DIR%\bolt.lnk" >> create_shortcut.vbs
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> create_shortcut.vbs
-echo oLink.TargetPath = "%CURRENT_DIR%\run.bat" >> create_shortcut.vbs
-echo oLink.WorkingDirectory = "%CURRENT_DIR%" >> create_shortcut.vbs
-echo oLink.IconLocation = "%CURRENT_DIR%\icons\bolt.ico" >> create_shortcut.vbs
-echo oLink.Save >> create_shortcut.vbs
-cscript //nologo create_shortcut.vbs
-del create_shortcut.vbs
-
-if exist "%CURRENT_DIR%\bolt.lnk" (
-    echo Shortcut created successfully at: %CURRENT_DIR%\bolt.lnk
-) else (
-    echo Failed to create shortcut!
-)
-
-echo Installation complete! You can now run the application by double-clicking bolt.lnk
+echo Installation complete!
 pause
