7eded7d85ffcb3be55ac94449f4b1f61f4f80500
Beekorn <162875479+beekorn@users.noreply.github.com>
2025-02-22 15:11:57 +0800

    Add files via upload

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -290,8 +290,8 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
         <div ref={scrollRef} className="flex flex-col lg:flex-row overflow-y-auto w-full h-full">
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
-              <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
+              <div id="intro" className="mt-[8vh] max-w-chat mx-auto text-center px-4 lg:px-0">
+                <h1 className="text-xl lg:text-3xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
                   Let Bolt.Star help you code like a star
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
